5bb9a824ee308f203f165ebe23653ae3482365ac
guilhermedelucas <gdelucasde@gmail.com>
2023-01-21 08:14:26 +0100

    Added sanity studio

---
+++ theme.ts
@@ -0,0 +1,10 @@
+import { buildLegacyTheme } from "sanity";
+
+const props = {
+	"--white": "#fff",
+	"--black": "#1a1a1a",
+	"--brand": "#F7AB0A",
+	"--red": "#db4437",
+	"--yellow": "#f4b400",
+	"--green": "@0f9d58",
+};
